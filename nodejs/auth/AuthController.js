b2b46bd141027c6d5aa654a2601f2f7c79177f11
Aristide legba <aristidelegba67@gmail.com>
2021-03-25 09:09:51 +0100

    latest changes

--- nodejs/auth/AuthController.js
+++ nodejs/auth/AuthController.js
@@ -35,7 +35,12 @@ router.get('/me', function (req, res) {
   jwt.verify(token, config.secret, function (err, decoded) {
     if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
 
-    res.status(200).send(decoded);
+    User.findById(decoded.id, function (err, user) {
+      if (err) return res.status(500).send("There was a problem finding the user.");
+      if (!user) return res.status(404).send("No user found.");
+      user.password = bcrypt.decodeBase64(user.password, 8)
+      res.status(200).send(user);
+    });
   });
 });
 
